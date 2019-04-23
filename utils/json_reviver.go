424fe821e2b3b49638f4f1c441e80c29b6c34155
Berk Soysal <Berk.Soysal@ibm.com>
2019-04-23 19:13:57 -0400

    Initial stream parsing of TxnLogs

---
+++ utils/json_reviver.go
@@ -0,0 +1,78 @@
+package odata
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+)
+
+// JSONReviver is the data type used to parse JSON streams.
+type JSONReviver struct {
+	decoder *json.Decoder
+}
+
+// NewJSONReviver returns a new JSON reviver.
+func NewJSONReviver(stream io.Reader) *JSONReviver {
+	r := new(JSONReviver)
+	r.decoder = json.NewDecoder(stream)
+
+	return r
+}
+
+// ParseTransactionLogs parses an incoming stream response thath contains transaction log entries.
+func (r *JSONReviver) ParseTransactionLogs(callback func(*TransactionLogEntry)) error {
+	t, err := r.decoder.Token()
+	if err != nil {
+		return err
+	}
+
+	if delim, ok := t.(json.Delim); !ok || delim != '{' {
+		return errors.New("JSON object start delimiter not found")
+	}
+
+	for r.decoder.More() {
+		token, err := r.decoder.Token()
+		if err != nil {
+			return err
+		}
+
+		// Skip other fields than 'value' for simplicity
+		if token != "value" {
+			continue
+		}
+
+		// 'value' should contain an array
+		token, err = r.decoder.Token()
+		if err != nil {
+			return err
+		}
+
+		if delim, ok := token.(json.Delim); !ok || delim != '[' {
+			return errors.New("JSON array start delimiter not found")
+		}
+
+		// Read array elements
+		for r.decoder.More() {
+			// Read next item (large object)
+			txnLog := TransactionLogEntry{}
+			err := r.decoder.Decode(&txnLog)
+			if err != nil {
+				return errors.New("unable to decode transaction log entry")
+			}
+
+			// Give transactionLog to the callback for processing.
+			callback(&txnLog)
+		}
+		// End of Array
+		token, err = r.decoder.Token()
+		if err != nil {
+			return err
+		}
+
+		if delim, ok := token.(json.Delim); !ok || delim != ']' {
+			return errors.New("JSON array end delimiter not found")
+		}
+	}
+
+	return nil
+}
